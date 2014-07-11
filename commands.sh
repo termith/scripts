6f669a58262de0ab5ca68762ab35bbaa0348be98
ddemidov <ddemidov@2reallife.com>
2014-07-11 15:47:44 +0400

    added mp3-tag conversion

---
+++ commands.sh
@@ -0,0 +1,14 @@
+# Справочник по командам и полезным опциям
+
+dpkg -i --force-confmiss $package_deb # Переустановка с восстановлением конфига
+
+apt-get install --reinstall -o Dpkg::Options::="--force-confmiss" $package_name 
+
+cut -d: -f1 /etc/passwd # Все пользователи системы
+
+grep --binary-files=text --context=n 'stringfromfile' /dev/somePartition > someFile # Восстановление удаленных файлов
+																					# Сохраняет n строк после stringfromfile 
+find -iname '*.mp3' -print0 | xargs -0 mid3iconv -eCP1251 --remove-v1 				# Меняем кодировку музыки
+
+																					
+
