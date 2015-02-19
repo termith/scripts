f267802a57bd07c2d929f7fe3de6e3949dbdbf27
Dmitry Demidov <dmitrydemidov@dmitrys-mbp.ru.corp.acronis.com>
2015-02-19 11:02:36 +0300

    file generate added

--- commands.sh
+++ commands.sh
@@ -7,8 +7,10 @@ apt-get install --reinstall -o Dpkg::Options::="--force-confmiss" $package_name
 cut -d: -f1 /etc/passwd # Все пользователи системы
 
 grep --binary-files=text --context=n 'stringfromfile' /dev/somePartition > someFile # Восстановление удаленных файлов
-																					# Сохраняет n строк после stringfromfile 
-find -iname '*.mp3' -print0 | xargs -0 mid3iconv -eCP1251 --remove-v1 				# Меняем кодировку музыки
+# Сохраняет n строк после stringfromfile 
 
+find -iname '*.mp3' -print0 | xargs -0 mid3iconv -eCP1251 --remove-v1 	# Меняем кодировку музыки
+
+head -c bytes_size /dev/random > my_file & # Генерирует файл указанного размера, выполняется в фоне (&)
 																					
 
